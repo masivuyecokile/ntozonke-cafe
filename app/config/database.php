0b2e18aa536e43709c613c2645d177d4e4f4145a
Masivuye Cokile <masivuyecokile@gmail.com>
2026-07-07 16:58:18 +0200

    new file:   app/config/database.php

---
+++ app/config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database
+{
+    private string $host = 'localhost';
+    private string $dbName = 'ntozonke_cafe';
+    private string $username = 'root';
+    private string $password = '';
+    private ?PDO $conn = null;
+
+    public function connect(): PDO
+    {
+        if ($this->conn === null) {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbName};charset=utf8mb4";
+
+            $this->conn = new PDO($dsn, $this->username, $this->password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
+
+        return $this->conn;
+    }
+}
