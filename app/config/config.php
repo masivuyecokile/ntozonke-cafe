3a5a4d8246f07ede0d585b043a075d50a6ba8ff6
Masivuye Cokile <masivuyecokile@gmail.com>
2026-07-07 14:33:35 +0200

    added config file

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('APP_NAME', 'Internet Cafe Management');
+define('BUSINESS_NAME', 'Ntozonke Internet Cafe');
+
+define('BASE_URL', 'http://localhost/ntozonke-cafe/public');
+
+date_default_timezone_set('Africa/Johannesburg');
